3386f3af88fa1f8e3fe8194dd39fa5b4e6113d5b
dttrung257 <77710663+dttrung257@users.noreply.github.com>
2021-09-28 20:01:30 +0700

    Version 1.1

--- src/DictionaryManagement.java
+++ src/DictionaryManagement.java
@@ -2,17 +2,24 @@ import java.util.*;
 
 public class DictionaryManagement
 {
+    /** Insert word from commandline.*/
     public static void insertFromCommandline() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String str = sc.nextLine();
         for (int i = 0; i < n; i++) {
-            Word w = new Word();
+            Word word = new Word();
             str = sc.nextLine();
-            w.setWord_target(str);
+            String firstLetterE = str.substring(0, 1);
+            String remainingLettersE = str.substring(1, str.length());
+            str = firstLetterE.toUpperCase() + remainingLettersE.toLowerCase();
+            word.setWord_target(str);
             str = sc.nextLine();
-            w.setWord_explain(str);
-            Dictionary.words[Dictionary.sz++] = w;
+            String firstLetterV = str.substring(0, 1);
+            String remainingLettersV = str.substring(1, str.length());
+            str = firstLetterV.toUpperCase() + remainingLettersV.toLowerCase();
+            word.setWord_explain(str);
+            Dictionary.words.add(word);
         }
 
     }
