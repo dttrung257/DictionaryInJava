680d7a4f3ad5bc7c08e69441d654eebc945236bc
dttrung257 <77710663+dttrung257@users.noreply.github.com>
2021-09-28 20:02:17 +0700

    Version 1.1

--- src/DictionaryCommandline.java
+++ src/DictionaryCommandline.java
@@ -1,18 +1,21 @@
 public class DictionaryCommandline {
 
+    /** Show all words. */
     public static void showAllWords() {
         System.out.printf("%-4s%-15s%s\n", "No", "Tieng Anh", "Tieng Viet");
         System.out.println("-------------------------------------");
-        for (int i = 0; i < Dictionary.sz; i++) {
-            System.out.printf("%-4s%-15s%s\n", i+1, Dictionary.words[i].getWord_target(), Dictionary.words[i].getWord_explain());
+        for (int i = 0; i < Dictionary.words.size(); i++) {
+            System.out.printf("%-4s%-15s%s\n", i+1, Dictionary.words.get(i).getWord_target(), Dictionary.words.get(i).getWord_explain());
         }
     }
+
+    /** Basic function. */
     public static void dictionaryBasic()
     {
         DictionaryManagement.insertFromCommandline();
         showAllWords();
     }
-
+    /** Main. */
     public static void main(String args[]) {
         Dictionary dict = new Dictionary();
         dictionaryBasic();
